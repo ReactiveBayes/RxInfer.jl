1e42bb6cd92b95ff037c83f3d5c788af5363c813
Bagaev Dmitry <bvdmitri@gmail.com>
2022-08-30 16:49:09 +0200

    add linear regression example

--- examples/.meta.jl
+++ examples/.meta.jl
@@ -22,6 +22,12 @@ return [
         hidden = false
     ),
     (
+        path = "Linear Regression.ipynb",
+        title = "Bayesian Linear Regression",
+        description = "An example of Bayesian linear regression.",
+        hidden = false
+    ),
+    (
         path = "Assessing People Skills.ipynb",
         title = "Assessing People’s Skills",
         description = "The demo is inspired by the example from Chapter 2 of Bishop's Model-Based Machine Learning book. We are going to perform an exact inference to assess the skills of a student given the results of the test.",
