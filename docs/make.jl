aae34716d0a3096d68b062bf894907e58b842efd
Bagaev Dmitry <bvdmitri@gmail.com>
2022-10-03 19:07:03 +0200

    update theme

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,18 @@
 using RxInfer
 using Documenter
 
+## Custom theme building
+
+using Sass
+
+assets = joinpath(@__DIR__, "src", "assets")
+ispath(assets) || mkpath(assets)
+
+themesrc = joinpath(@__DIR__, "theme", "theme.scss")
+themedst = joinpath(assets, "theme.css")
+    
+Sass.compile_file(themesrc, themedst)
+
 ## https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988
 ## https://gr-framework.org/workstations.html#no-output
 ENV["GKSwstype"] = "100"
@@ -13,7 +25,16 @@ Examples = map(filter((f) -> f != "Overview.md", readdir(ExamplesPath))) do exam
     return replace(example, ".md" => "") => joinpath("examples", example)
 end
 
+# struct DocumentationWriter <: Documenter.Writer
+#     base :: Documenter.HTML
+# end
+
+# function Documenter.Selectors.runner(::Type{T}, writer::DocumentationWriter, document) where {T}
+#     Documenter.Selectors.runner(T, writer.base, document)
+# end
+
 makedocs(;
+    draft = true,
     modules = [ RxInfer ],
     authors = "Bagaev Dmitry <d.v.bagaev@tue.nl> and contributors",
     repo = "https://github.com/biaslab/RxInfer.jl/blob/{commit}{path}#{line}",
@@ -22,7 +43,10 @@ makedocs(;
         prettyurls = get(ENV, "CI", "false") == "true",
         canonical = "https://biaslab.github.io/RxInfer.jl",
         edit_link = "main",
-        assets = String[]
+        assets = String[ "assets/theme.css" ],
+        footer = """
+        <b>Privet</b>
+        """
     ),
     pages = [
         "Home"     => "index.md",
