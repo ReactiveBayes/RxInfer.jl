a1303d7c584f1ec61a022767084839c5bfcd5320
Bagaev Dmitry <bvdmitri@gmail.com>
2022-10-04 19:18:16 +0200

    fix header

--- docs/src/assets/header.js
+++ docs/src/assets/header.js
@@ -1,9 +1,14 @@
 
 // We add a simple `onload` hook to inject the custom header for our `HTML`-generated pages
 window.onload = function() {
-    const header = document.createElement('p');
+    const header = document.createElement('div');
+    const link = document.createElement('a')
 
-    header.innerText = 'RxInfer Header Element';
+    link.href = "https://github.com/biaslab/RxInfer.jl"
+    link.innerText = "Open GitHub repository"
+
+    header.classList.add('rx-infer-header')
+    header.appendChild(link)
     
     const documenterTarget = document.querySelector('#documenter');
     
