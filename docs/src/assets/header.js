0b421037ce517185b3c62590f7a4d92273010719
Bagaev Dmitry <bvdmitri@gmail.com>
2022-10-14 14:16:11 +0200

    docs: Simplify header

--- docs/src/assets/header.js
+++ docs/src/assets/header.js
@@ -5,66 +5,55 @@ window.onload = function() {
     const header = document.createElement('header')
     header.classList.add("navigation")
     header.appendChild((() => {
-        // <div class="container">
         const container = document.createElement('div')
         container.classList.add('container')
         container.appendChild((() => {
-            // <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
             const nav = document.createElement('nav')
-            nav.classList.add("navbar", "navbar-expand-lg", "navbar-light", "bg-transparent")
-
-            // Brand logo already exists in the documentation
-            // nav.appendChild((() => {
-            //     const brand = document.createElement('a')
-            //     brand.classList.add("navbar-brand")
-            //     brand.href = "https://rxinfer.ml"
-            //     brand.appendChild((() => {
-            //         const brandimg = document.createElement('img')
-            //         brandimg.width = 100
-            //         brandimg.height = '100%'
-            //         brandimg.classList.add("img-fluid")
-            //         brandimg.src = "assets/biglogo.svg"
-            //         brandimg.alt = "RxInfer website"
-            //         return brandimg
-            //     })())
-            //     return brand
-            // })())
+            nav.classList.add("navbar")
 
             nav.appendChild((() => {
-                const collapse = document.createElement("div")
-                collapse.classList.add("collapse", "navbar-collapse", "text-center")
+                const ul = document.createElement("ul")
+                ul.classList.add("navbar-nav")
 
-                collapse.appendChild((() => {
-                    const ul = document.createElement("ul")
-                    ul.classList.add("navbar-nav", "mx-auto")
-
-                    const items = [
-                        { title: "Get Started", link: "poka" },
-                        { title: "Documentation", link: "poka" },
-                        { title: "Examples", link: "poka" },
-                        { title: "Papers", link: "poka" },
-                        { title: "Team", link: "poka" },
-                        { title: "GitHub", link: "poka" },
-                    ]
-
-                    items.forEach((item) => {
-                        ul.appendChild(((item) => {
-                            const li = document.createElement("li")
-                            li.classList.add("nav-item")
-                            li.appendChild((() => {
-                                const a = document.createElement("a")
-                                a.classList.add("nav-link")
-                                a.href = item.link
-                                a.innerHTML = item.title
-                                a.title = item.title
-                                return a
-                            })())
-                            return li
-                        })(item))
-                    })
-                    return ul
+                ul.appendChild((() => {
+                    const smalllink = document.createElement('li')
+                    smalllink.classList.add('small-item')
+                    smalllink.appendChild((() => {
+                        const a = document.createElement('a')
+                        a.classList.add("nav-link")
+                        a.href = 'https://rxinfer.ml'
+                        a.innerHTML = 'RxInfer.jl'
+                        a.title = 'RxInfer.jl'
+                        return a
+                    })())
+                    return smalllink
                 })())
-                return collapse
+
+                const items = [
+                    { title: "Get Started", link: "poka" },
+                    { title: "Documentation", link: "poka" },
+                    { title: "Examples", link: "poka" },
+                    { title: "Papers", link: "poka" },
+                    { title: "Team", link: "poka" },
+                    { title: "GitHub", link: "poka" },
+                ]
+
+                items.forEach((item) => {
+                    ul.appendChild(((item) => {
+                        const li = document.createElement("li")
+                        li.classList.add("nav-item")
+                        li.appendChild((() => {
+                            const a = document.createElement("a")
+                            a.classList.add("nav-link")
+                            a.href = item.link
+                            a.innerHTML = item.title
+                            a.title = item.title
+                            return a
+                        })())
+                        return li
+                    })(item))
+                })
+                return ul
             })())
             return nav
         })())
