6eb9dabda7c85679077fb3cb2d95050ef4e64bc5
Bagaev Dmitry <bvdmitri@gmail.com>
2022-12-19 12:10:12 +0100

    add example

---
+++ paper/paper.jl
@@ -0,0 +1,93 @@
+# This file contains an example code for the `paper.md` for Journal of Open Source Software
+
+using RxInfer, StableRNGs, Plots
+
+Δt = 0.01 # -- Time step resolution
+G = 9.81  # -- Gravitational acceleration
+
+# Nonlinear state-transition function `f`
+g(x) = [x[1] + x[2]*Δt, x[2] - G*sin(x[1])*Δt]
+
+@model function pendulum()
+    # Define reactive inputs for the `prior`
+    prior_mean = datavar(Vector{Float64})
+    prior_cov  = datavar(Matrix{Float64})
+
+    # Define the `prior` state
+    state_prior ~ MvNormal(μ = prior_mean, Σ = prior_cov)
+    # Use `f` as state transition function
+    state ~ g(state_prior)
+    
+    # Assign a prior for the noise component
+    noise ~ Gamma(1.0, 10.0)
+
+    # Define reactive input for the `observation`
+    observation = datavar(Float64)
+    observation ~ Normal(μ = dot([ 1.0, 0.0 ], state), τ = noise)  
+end
+
+@constraints function pendulum_constraint()
+    # Assume the `state` and the `noise`
+    # are independent
+    q(state, noise) = q(state)q(noise)
+end
+
+@meta function pendulum_meta()
+    # Use the `Linearization` approximation method 
+    # around the (potentially) non-linear function `f`
+    g() -> Linearization()
+end
+
+# Generate dummy data
+function dataset(; T, precision = 1.0, seed = 42)
+    rng = StableRNG(seed)
+
+    # Initialize data array
+    timesteps = (1:T).*Δt
+    states = zeros(2,T)
+    observations = zeros(T,)
+
+    # Initial states
+    states[:, 1] = [ 1.0 , 0.0 ]
+
+    for t = 2:T
+        # State transition
+        states[:, t] = g(states[:, t - 1])
+        
+        # Observation likelihood, we observe only the first component
+        observations[t] = rand(rng, NormalMeanPrecision(states[1,t][1], precision))
+    end
+
+    # We return `states` only for testing purposes
+    return timesteps, states, observations
+end
+
+timesteps, states, observations = dataset(T = 1000, precision = 0.1)
+
+plot(timesteps, states[1, :])
+scatter!(timesteps, observations, ms = 2, alpha = 0.5)
+
+function experiment(observations)
+
+    autoupdates = @autoupdates begin 
+        # Update `prior` automatically as soon as 
+        # we have a new posterior for the `state`
+        (prior_mean, prior_cov) = mean_cov(q(state))
+    end
+
+    results = rxinference(
+        model = pendulum(),
+        constraints = pendulum_constraint(),
+        meta = pendulum_meta(), 
+        data = (observation = observations, ),
+        initmarginals = (state = vague(MvNormalMeanPrecision, 2), ),
+        historyvars = (state = KeepLast(), ),
+        keephistory = length(observations),
+        iterations = 5,
+        autostart = true
+    )
+
+    return results
+end
+
+results = experiment(observations)
