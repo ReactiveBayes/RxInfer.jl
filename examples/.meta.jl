239e3929d2c1367d86799e835171903038775d2b
Bagaev Dmitry <bvdmitri@gmail.com>
2023-06-27 09:14:56 +0200

    fix ci

--- examples/.meta.jl
+++ examples/.meta.jl
@@ -57,7 +57,7 @@ return [
     ),
     (
         path  = "Hidden Markov Model.ipynb",
-        title = "Ensemble Learning of a Hidden Markov Model",
+        title = "How to train your Hidden Markov Model",
         description = "An example of structured variational Bayesian inference in Hidden Markov Model with unknown transition and observational matrices.",
         hidden = false
     ),
