fd92f695d818871506b236667aa03ab40b189871
Bagaev Dmitry <bvdmitri@gmail.com>
2023-08-01 13:08:05 +0200

    style: make format

--- scripts/examples.jl
+++ scripts/examples.jl
@@ -185,11 +185,7 @@ function Base.run(examplesrunner::ExamplesRunner)
     close(examplesrunner.resultschannel)
 
     # Fix paths from the `pics/` folder located in the examples
-    fixpics = (
-        "![](pics/" => "![](../../assets/examples/pics/", 
-        "![](./pics/" => "![](../../assets/examples/pics/",
-        "![](../pics/" => "![](../../assets/examples/pics/"
-    )
+    fixpics = ("![](pics/" => "![](../../assets/examples/pics/", "![](./pics/" => "![](../../assets/examples/pics/", "![](../pics/" => "![](../../assets/examples/pics/")
 
     if isnothing(examplesrunner.specific_example)
 
