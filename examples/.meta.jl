4a229c9747157bed344ba2dd2408edb75e035a98
Bagaev Dmitry <bvdmitri@gmail.com>
2023-08-02 21:12:10 +0200

    fix SNN title

--- examples/.meta.jl
+++ examples/.meta.jl
@@ -143,8 +143,8 @@ return (
             category = :problem_specific
         ),
         (
-            filename = "Simple nonlinear node.ipynb",
-            title = "Custom nonlinear node",
+            filename = "Simple Nonlinear Node.ipynb",
+            title = "Simple Nonlinear Node",
             description = "In this example we create a non-conjugate model and use a nonlinear link function between variables. We show how to extend the functionality of `RxInfer` and to create a custom factor node with arbitrary message passing update rules.",
             category = :problem_specific
         ),
