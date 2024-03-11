747c4a6f7298f5ba2386c59cffb58ceccd4a658f
Bagaev Dmitry <bvdmitri@gmail.com>
2024-03-11 13:15:24 +0100

    2prev

--- src/graphppl.jl
+++ src/graphppl.jl
@@ -1,6 +1,11 @@
 import GraphPPL
 import ExponentialFamily
 
+GraphPPL.factor_alias(::Type{Normal}, ::Val{(:mean, :var)}) = ExponentialFamily.NormalMeanVariance
+GraphPPL.factor_alias(::Type{Normal}, ::Val{(:mean, :variance)}) = ExponentialFamily.NormalMeanVariance
+GraphPPL.factor_alias(::Type{Normal}, ::Val{(:m, :v)}) = ExponentialFamily.NormalMeanVariance
+GraphPPL.factor_alias(::Type{Normal}, ::Val{(:μ, :v)}) = ExponentialFamily.NormalMeanVariance
+
 GraphPPL.factor_alias(::Type{MvNormal}, ::Val{(:mean, :covariance)}) = ExponentialFamily.MvNormalMeanCovariance
 GraphPPL.factor_alias(::Type{MvNormal}, ::Val{(:μ, :Σ)}) = ExponentialFamily.MvNormalMeanCovariance
 
