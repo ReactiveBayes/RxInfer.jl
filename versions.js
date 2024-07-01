b79ffa7c6b0be768e1ccf0285f0f5a586c75aff5
Bagaev Dmitry <bvdmitri@gmail.com>
2024-07-01 08:26:09 +0000

    build based on 35456e3

---
+++ versions.js
@@ -0,0 +1,14 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v3.4",
+  "v3.3",
+  "v3.2",
+  "v3.1",
+  "v3.0",
+  "v2.17",
+  "v2.16",
+  "v2.15",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v3.4.0";
+var DOCUMENTER_STABLE = "stable";
