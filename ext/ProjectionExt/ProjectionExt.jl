f265239a6d3d76ea3f4b3bf7dedd5164103b863c
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-14 17:57:10 +0300

    wip revision

--- ext/ProjectionExt/ProjectionExt.jl
+++ ext/ProjectionExt/ProjectionExt.jl
@@ -4,6 +4,8 @@ using RxInfer, ExponentialFamily, ExponentialFamilyProjection, ReactiveMP, Bayes
 
 ReactiveMP.default_form_check_strategy(::ProjectedTo) = FormConstraintCheckLast()
 ReactiveMP.default_prod_constraint(::ProjectedTo) = GenericProd()
+ReactiveMP.default_form_check_strategy(::Union{Vector{<:ProjectedTo}, Tuple}) = FormConstraintCheckLast()
+ReactiveMP.default_prod_constraint(::Union{Vector{<:ProjectedTo}, Tuple})  = GenericProd()
 
 mutable struct ProjectionContext{T}
     previous::Union{Nothing, T}
@@ -12,6 +14,24 @@ end
 function ReactiveMP.prepare_context(constraint::ProjectedTo)
     T = ExponentialFamilyProjection.get_projected_to_type(constraint)
     return ProjectionContext{T}(nothing)
+end
+
+function ReactiveMP.prepare_context(constraint::Union{Vector{<:ProjectedTo}, Tuple}) 
+    T = map(ExponentialFamilyProjection.get_projected_to_type, constraint)
+    return map(t -> ProjectionContext{t}(nothing), T)
+end
+
+function initial_point_from_context_constraint(previous_context, constraint)
+    if !isnothing(previous_context)
+        initialη = getnaturalparameters(convert(ExponentialFamilyDistribution, previous_context))
+        manifold = ExponentialFamilyProjection.get_projected_to_manifold(constraint)
+        initialpoint = ExponentialFamilyProjection.ExponentialFamilyManifolds.partition_point(manifold, initialη)
+
+        return initialpoint
+
+    else
+        return nothing
+    end
 end
 
 function ReactiveMP.constrain_form(constraint::ProjectedTo, context::ProjectionContext, something::Distribution)
@@ -27,19 +47,28 @@ function ReactiveMP.constrain_form(constraint::ProjectedTo, context::ProjectionC
 end
 
 function ReactiveMP.constrain_form(constraint::ProjectedTo, context::ProjectionContext, something::ProductOf)
-    return ReactiveMP.constrain_form(constraint::ProjectedTo, context, (x) -> logpdf(something, x))
+    left  = BayesBase.getleft(something)
+    right = BayesBase.getright(something)
+
+    left_logpdf  = typeof(left) <: Distribution ? x -> logpdf(left, x) : left
+    right_logpdf = typeof(right) <: Distribution ? x-> logpdf(right, x) : right
+    return ReactiveMP.constrain_form(constraint::ProjectedTo, context, (x) -> left_logpdf(x) + right_logpdf(x) )
 end
 
+
 function ReactiveMP.constrain_form(constraint::ProjectedTo, context::ProjectionContext, fn)
-    initialpoint = nothing
-    if !isnothing(context.previous)
-        initialη = getnaturalparameters(convert(ExponentialFamilyDistribution, context.previous))
-        manifold = ExponentialFamilyProjection.get_projected_to_manifold(constraint)
-        initialpoint = ExponentialFamilyProjection.ExponentialFamilyManifolds.partition_point(manifold, initialη)
-    end
+    initialpoint = initial_point_from_context_constraint(context.previous, constraint)
     result = project_to(constraint, fn; initialpoint = initialpoint)
     context.previous = result
     return result
 end
+
+function ReactiveMP.constrain_form(constraints::Vector{<:ProjectedTo}, contexts::Vector{<:ProjectionContext}, fn::Base.Generator) 
+    length_constraints = length(constraints)
+    result = map((f, constraint, context) -> project_to(constraint, f; initialpoint =initial_point_from_context_constraint(context.previous, constraint) ) , fn, constraints, contexts)
+    map((r,c) -> c.previous = r , result, contexts)
+    return FactorizedJoint(ntuple(i -> result[i] , length_constraints))
+end
+
 
 end
