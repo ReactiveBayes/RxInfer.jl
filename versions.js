aa2258dc8bc768c13689407f28fe60ec37016502
Bagaev Dmitry <bvdmitri@gmail.com>
2024-08-05 08:32:58 +0000

    build based on 858000c

---
+++ versions.js
@@ -0,0 +1,15 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v3.5",
+  "v3.4",
+  "v3.3",
+  "v3.2",
+  "v3.1",
+  "v3.0",
+  "v2.17",
+  "v2.16",
+  "v2.15",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v3.5.0";
+var DOCUMENTER_STABLE = "stable";
