64060e187587eafd231ea54215800b19840531c8
Bagaev Dmitry <bvdmitri@gmail.com>
2025-01-14 15:13:49 +0000

    build based on fe00961

---
+++ versions.js
@@ -0,0 +1,19 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v3.9",
+  "v3.8",
+  "v3.7",
+  "v3.6",
+  "v3.5",
+  "v3.4",
+  "v3.3",
+  "v3.2",
+  "v3.1",
+  "v3.0",
+  "v2.17",
+  "v2.16",
+  "v2.15",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v3.9.0";
+var DOCUMENTER_STABLE = "stable";
