894ad869314270773ba30c923cd4c2aa487e01fe
Bagaev Dmitry <bvdmitri@gmail.com>
2025-03-05 14:59:32 +0000

    build based on a7687f8

--- versions.js
+++ versions.js
@@ -1,5 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
+  "v4.2",
+  "v4.1",
   "v4.0",
   "v3.10",
   "v3.9",
@@ -17,5 +19,5 @@ var DOC_VERSIONS = [
   "v2.15",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v4.0.1";
+var DOCUMENTER_NEWEST = "v4.2.0";
 var DOCUMENTER_STABLE = "stable";
