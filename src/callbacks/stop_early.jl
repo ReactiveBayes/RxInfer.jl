ffc793667e020f7fa54847e5506b306eb87056e1
Raphael-Tresor <40422324+Raphael-Tresor@users.noreply.github.com>
2026-02-10 15:42:31 +0100

    use isapprox

--- src/callbacks/stop_early.jl
+++ src/callbacks/stop_early.jl
@@ -1,18 +1,41 @@
 export StopEarlyIterationStrategy
 
-#TODO we should give an option for rtol and atol, right now it's atol
 
+"""
+   StopEarlyIterationStrategy
+
+Early-stopping criterion based on consecutive Bethe free energy (FE) values at each itertion.
+
+Fields
+- `atol::Float64`: Absolute tolerance 
+- `rtol::Float64`: Relative tolerance 
+- `start_fe_value::Float64`: Initial FE value used for the first comparison (optional)
+- `fe_values::Vector{Float64}`: History of observed FE values (most recent is last).
+
+The iterations stop when the current FE is approximately equal to the previous FE
+under the given tolerances.
+"""
 struct StopEarlyIterationStrategy
-    tolerance::Float64
+    atol::Float64
+    rtol::Float64
     start_fe_value::Float64
     fe_values::Vector{Float64}
 end
 
 function StopEarlyIterationStrategy(tolerance::Float64) 
-	return StopEarlyIterationStrategy(tolerance, Inf, [])
+	return StopEarlyIterationStrategy(tolerance, Inf, Inf, [])
 end
 
-function (strategy::StopEarlyIterationStrategy)(model, niteration)
+
+
+"""
+    (strategy::StopEarlyIterationStrategy)(model) -> Bool
+
+Returns:
+- `true` if the current FE is approximately is approximately equal to the previous FE (stop iterations),
+  `false` otherwise (continue iterations).
+"""
+function (strategy::StopEarlyIterationStrategy)(model)
     current_fe_value = 0.0
     # Subscribe on the `BetheFreeEnergy` stream but only `take(1)` value from it
     subscribe!(score(model, RxInfer.BetheFreeEnergy(Real), RxInfer.DefaultObjectiveDiagnosticChecks) |> take(1), (v) -> current_fe_value = v)
@@ -21,9 +44,5 @@ function (strategy::StopEarlyIterationStrategy)(model, niteration)
     # Save the current value in the history
     push!(strategy.fe_values, current_fe_value)
     # Stop early if the previous value is close to the current
-    if abs(previous_fe_value - current_fe_value) < strategy.tolerance
-        return true
-    end
-    # Otherwise continue
-    return false
+    return isapprox(current_fe_value, previous_fe_value; atol=strategy.atol, rtol=strategy.rtol)
 end
