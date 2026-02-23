f294c0d40e011ded07d8d5e110a09c200629e1b8
Bagaev Dmitry <bvdmitri@gmail.com>
2026-02-23 08:45:01 +0000

    build based on d1c3313

--- versions.js
+++ versions.js
@@ -1,5 +1,10 @@
 var DOC_VERSIONS = [
   "stable",
+  "v4.7",
+  "v4.6",
+  "v4.5",
+  "v4.4",
+  "v4.3",
   "v4.2",
   "v4.1",
   "v4.0",
@@ -19,5 +24,5 @@ var DOC_VERSIONS = [
   "v2.15",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v4.2.0";
+var DOCUMENTER_NEWEST = "v4.7.0";
 var DOCUMENTER_STABLE = "stable";
