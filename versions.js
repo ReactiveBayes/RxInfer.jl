c14cd2486a89d3e69a117e31cfd9aa4719eb724a
Bagaev Dmitry <bvdmitri@gmail.com>
2026-04-17 14:15:25 +0000

    build based on 409bac3

--- versions.js
+++ versions.js
@@ -1,5 +1,11 @@
 var DOC_VERSIONS = [
   "stable",
+  "v5.0",
+  "v4.7",
+  "v4.6",
+  "v4.5",
+  "v4.4",
+  "v4.3",
   "v4.2",
   "v4.1",
   "v4.0",
@@ -19,5 +25,5 @@ var DOC_VERSIONS = [
   "v2.15",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v4.2.0";
+var DOCUMENTER_NEWEST = "v5.0.0";
 var DOCUMENTER_STABLE = "stable";
