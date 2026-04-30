c6a4865d5256fe5c5b9867a5e1ac686556a3cd29
Fons van der Plas <fonsvdplas@gmail.com>
2026-04-30 16:10:51 +0200

    whitespace: run formatter

--- src/callbacks/perfetto.jl
+++ src/callbacks/perfetto.jl
@@ -107,7 +107,6 @@ _to_observability_string(::ReactiveMP.MessageProductContext, _keyname::Symbol) =
 _to_observability_string(::ReactiveMP.MessageMapping, _keyname::Symbol) = "<omitted>"
 _to_observability_string(::ProbabilisticModel, _keyname::Symbol) = "<omitted>"
 
-
 # Converts a `time_ns()` timestamp to a wall-clock `DateTime` by computing the
 # drift between Julia's monotonic clock and the Unix epoch at the moment of the call.
 function _time_ns_to_datetime(t::UInt64)::Dates.DateTime
