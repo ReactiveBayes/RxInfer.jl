cae3fdcc87cc8bcca610f4fd637989464df1fd14
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-05-18 13:20:25 +0530

    fix: reduce gmm_multivariate test samples and fix runtests.jl map bug (#656)

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ end
 if isempty(ARGS)
     @run_package_tests(verbose = true)
 else
-    filtered_files = map(arg -> join(split(arg, ":"), "/"))
+    filtered_files = map(arg -> join(split(arg, ":"), "/"), ARGS)
     filter_fn = ti -> any(occursin(ti.filename), filtered_files)
     @run_package_tests(filter = filter_fn, verbose = true)
 end
